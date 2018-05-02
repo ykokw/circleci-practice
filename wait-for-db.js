e11161227f0d2902b48dbfd8fcd686f47c476cb7
ykokw <0k4w4.yuk1@gmail.com>
2018-05-02 15:24:56 +0900

    Fixed

--- wait-for-db.js
+++ wait-for-db.js
@@ -14,7 +14,7 @@ const command = process.argv.reduce((cmd, arg, i) => {
   return cmd;
 }, process.argv[2]);
 
-const count = 0;
+let count = 0;
 const i = setInterval(() => {
   count++;
   if (count > 60) {
